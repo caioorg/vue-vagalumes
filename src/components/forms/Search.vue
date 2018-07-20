fbcab7c3729556db849614c377a79d54b7c331d4
Caio Almeida <caio.almeida@justdigital.com.br>
2018-07-19 23:42:00 -0300

    Comunicando com API do Vagalumes

--- src/components/forms/Search.vue
+++ src/components/forms/Search.vue
@@ -1,14 +1,22 @@
 <template>
+  <div>
     <div class="columns">
       <div class="column is-6 is-offset-3">
         <div class="field">
           <label class="label">Nome do artista:</label>
           <div class="control">
-            <input class="input" v-model="nameArtist" type="text" placeholder="Digite o nome do artista">
+            <input class="input" v-model.lazy="nameArtist" type="text" placeholder="Digite o nome do artista">
+            <button v-on:click="FilterArtist()">Buscar</button>
           </div>
         </div>
       </div>
     </div>
+    <div class="columns">
+      <div class="column items">
+        {{dados.url}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,11 +25,19 @@
     name: 'Search',
     data(){
       return{
-        nameArtist: ""
+        nameArtist: "",
+        dados: []
       }
     },
     methods:{
-      
+      FilterArtist(){
+        this.$http.get('https://www.vagalume.com.br/' + this.$options.filters.TrimReplace(this.nameArtist) + '/index.js').then(response => {
+            this.dados = response.body.artist;
+            console.log(response.body)
+        }, error =>{
+          console.log(error);
+        });
+      }
     },
     filters:{
       TrimReplace: (value) =>{        
